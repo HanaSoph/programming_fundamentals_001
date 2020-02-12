2aa93b4fde646de887f786c3ba6208b973340cc7
Hannah <hannahbeardmore1@hotmail.co.uk>
2020-02-12 15:44:19 +0000

    working through day 2 exercises

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,8 +4,45 @@ describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
-  
+
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns the price with a 50% discount applied", () => {
+    expect(utils.makeHalfPrice(60)).toBe(30);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns the total number of books", () => {
+    expect(utils.countBooks(["Harry Potter", "Little Women"])).toBe(2);
+  });
+});
+
+ describe ("utils.isInStock", ()=>{
+  test("returns true if the title is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+  
+    expect(utils.isInStock(book)).toBe(true);
+  });
+
+ })
